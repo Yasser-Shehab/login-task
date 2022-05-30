3846c3f57a32b67bdefb3263e6aebc047f358654
AquaApple <yassershehab9@gmail.com>
2022-05-30 15:23:38 +0200

    Added Routes and Slider Images

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Welcome from "./pages/Welcome/Welcome";
+import NotFound from "./pages/404/NotFound";
 
 function App() {
-  return <div className="App">Insta bug</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="welcome" element={<Welcome />} />
+          <Route path={"*"} element={<NotFound />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- src/constants/images.js
+++ src/constants/images.js
@@ -2,10 +2,16 @@ import github from "../assets/icons/github.svg";
 import google from "../assets/icons/google.svg";
 import instabug from "../assets/icons/Instabug.svg";
 import microsoft from "../assets/icons/microsoft.svg";
+import slider1 from "../assets/sliderImages/slider1.svg";
+import slider2 from "../assets/sliderImages/slider2.svg";
+import slider3 from "../assets/sliderImages/slider3.svg";
 
 export default {
   github,
   google,
   instabug,
   microsoft,
+  slider1,
+  slider2,
+  slider3,
 };
